4f5ea3bc87cb5bc4fd0b3884ffa5cd0e9753db51
knaroznik <Redivivatus@wp.pl>
2019-07-08 16:25:34 +0200

    [init/add] Character & Path implementation.

---
+++ Assets/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Path path;
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    private int currentPointID = 0;
+
+    private bool _moveEnabled = true;
+
+    private void Start()
+    {
+        this.transform.position = path.GetPosition(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_moveEnabled)
+        {
+            return;
+        }
+        float distance = Vector3.Distance(path.GetPosition(currentPointID), transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, path.GetPosition(currentPointID), Time.deltaTime * movementSpeed);
+
+        if(distance <= 1)
+        {
+            currentPointID++;
+        }
+
+        if(currentPointID >= path.Length())
+        {
+            _moveEnabled = false;
+        }
+    }
+}
---
+++ Assets/Path.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public Color pathColor = Color.white;
+
+    [HideInInspector]
+    public bool _drawGizmos = false;
+
+    private List<Transform> redirectedPath;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = pathColor;
+        Transform[] tempArray = GetComponentsInChildren<Transform>();
+        List<Transform> mainList = new List<Transform>();
+
+        foreach (Transform x in tempArray)
+        {
+            if (x != this.transform)
+            {
+                mainList.Add(x);
+            }
+        }
+
+        if (_drawGizmos)
+        {
+            for (int i = 0; i < mainList.Count; i++)
+            {
+                Vector3 position = mainList[i].position;
+                if (i > 0)
+                {
+                    Vector3 prevPosition = mainList[i - 1].position;
+                    Gizmos.DrawLine(prevPosition, position);
+                    Gizmos.DrawWireSphere(position, 0.3f);
+                }
+            }
+        }
+
+        mainList.Reverse();
+        redirectedPath = mainList;
+    }
+
+    private void CreatePath()
+    {
+        Transform[] tempArray = GetComponentsInChildren<Transform>();
+        List<Transform> mainList = new List<Transform>();
+
+        foreach (Transform x in tempArray)
+        {
+            if (x != this.transform)
+            {
+                mainList.Add(x);
+            }
+        }
+
+        mainList.Reverse();
+        redirectedPath = mainList;
+    }
+
+    public Vector3 GetPosition(int _value)
+    {
+        if(redirectedPath == null)
+        {
+            CreatePath();
+        }
+        return redirectedPath[_value].position;
+    }
+
+    public int Length()
+    {
+        return redirectedPath.Count;
+    }
+}

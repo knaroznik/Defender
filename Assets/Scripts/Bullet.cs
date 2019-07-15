8be1c336bf5008f4547e5bdf41be6bbe16afd822
knaroznik <Redivivatus@wp.pl>
2019-07-15 14:03:32 +0200

    [add] Tower.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this.transform.position + this.transform.forward, Time.deltaTime * speed);
+
+        //TODO : if to far destroy
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Character")
+        {
+            //CHECK SHAPE TODO
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject bulletPrefab;
+
+    private float reloadTime = 0.4f;
+    private float currentReloadTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+
+        if (Input.GetMouseButton(0) && currentReloadTime <= 0)
+        {
+            Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+            currentReloadTime = reloadTime;
+        }
+        else
+        {
+            currentReloadTime -= Time.deltaTime;
+        }
+
+
+    }
+
+    void Rotate()
+    {
+        Vector3 mouseWorldPosition = -Vector3.one;
+
+        Plane plane = new Plane(Vector3.up, 0f);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+
+        if (plane.Raycast(ray, out distance))
+        {
+            mouseWorldPosition = ray.GetPoint(distance);
+            this.gameObject.transform.LookAt(mouseWorldPosition);
+        }
+    }
+}
